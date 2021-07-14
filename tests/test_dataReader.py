a8334307a07eb0c82093f1812eaa7351f66ec07a
Aidan Payne <aidan@apayne.net>
2021-07-14 14:52:37 -0700

    Added test to raise csvError when passed object __next__ doesn't return strings as expected by csv.reader

--- tests/test_dataReader.py
+++ tests/test_dataReader.py
@@ -1,5 +1,7 @@
 import utils.dataReader
 from collections.abc import Iterable
+from csv import Error as csvError
+import pytest
 
 class mock_textFileObject():
     def __next__():
@@ -7,8 +9,17 @@ class mock_textFileObject():
     def __iter__(self):
         return self
 
+class mock_nontextFileObject():
+    def __next__():
+        return 2
+    def __iter__(self):
+        return self
 
 class Test_dataRead_Unit:
     
     def test_dataRead():
         assert isinstance(utils.dataReader.dataRead(mock_textFileObject()), Iterable)
+
+    def test_dataRead_invalidObject():
+        with pytest.raises(csvError):
+             assert isinstance(utils.dataReader.dataRead(mock_nontextFileObject()), Iterable)
