dbcc4bbca4e0f8044b6299719836c59402d63b29
Aidan Payne <aidan@apayne.net>
2021-07-14 14:46:52 -0700

    Renamed mock object to better reflect the input it mocks.
    Removed subclassing of Iterable, overkill when only __next__ and __iter__ are needed

--- tests/test_dataReader.py
+++ tests/test_dataReader.py
@@ -1,12 +1,14 @@
 import utils.dataReader
 from collections.abc import Iterable
 
-class mock_csvFile(Iterable):
+class mock_textFileObject():
     def __next__():
         return 'test'
+    def __iter__(self):
+        return self
 
 
 class Test_dataRead_Unit:
     
     def test_dataRead():
-        assert isinstance(utils.dataReader.dataRead(mock_csvFile()), Iterable)
+        assert isinstance(utils.dataReader.dataRead(mock_textFileObject()), Iterable)
