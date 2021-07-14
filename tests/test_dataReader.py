b5e013c8b39ca7abca1c3797469b1a7fb2b7fb9c
Aidan Payne <aidan@apayne.net>
2021-07-14 16:02:10 -0700

    Rewrote tests to cover success, file not exist, and binary file cases

--- tests/test_dataReader.py
+++ tests/test_dataReader.py
@@ -1,15 +1,16 @@
 import utils.dataReader
 from collections.abc import Iterable
-from csv import Error as csvError
+import csv
+from unittest.mock import mock_open, patch
 import pytest
 
-class mock_textFileObject():
-    def __next__():
+class mock_textFileObject:
+    def __next__(*args, **kwargs):
         return 'test'
     def __iter__(self):
         return self
 
-class mock_nontextFileObject():
+class mock_nontextFileObject:
     def __next__():
         return 2
     def __iter__(self):
@@ -17,9 +18,15 @@ class mock_nontextFileObject():
 
 class Test_dataRead_Unit:
     
-    def test_dataRead():
-        assert isinstance(utils.dataReader.dataRead(mock_textFileObject()), Iterable)
+    def test_dataRead(self, monkeypatch):
+        with patch("builtins.open", mock_open(read_data='text')):
+            assert isinstance(utils.dataReader.dataRead(mock_textFileObject), Iterable)
 
-    def test_dataRead_invalidObject():
-        with pytest.raises(csvError):
-             assert isinstance(utils.dataReader.dataRead(mock_nontextFileObject()), Iterable)
+    def test_dataRead_fileNotExist(self):
+        with pytest.raises(FileNotFoundError):
+            assert isinstance(utils.dataReader.dataRead('test.file'), Iterable)
+
+    def test_dataRead_binaryFile(self):
+        with pytest.raises(csv.Error):
+            with patch("builtins.open", mock_open(read_data=b'text')):
+                assert isinstance(utils.dataReader.dataRead(mock_nontextFileObject), Iterable)
