dee19c5b45e722ab8bb3626724e512c494b3ee27
Aidan Payne <aidan@apayne.net>
2021-07-14 19:22:51 -0700

    Initial implementation of encounterSplitter

--- utils/dataReader.py
+++ utils/dataReader.py
@@ -1,5 +1,6 @@
 import csv
 from typing import Iterable, TextIO
+from collections import Counter
 
 def dataRead(file: TextIO) -> list:
     with open(file, newline='') as source:
@@ -14,3 +15,34 @@ def dataPrint(data: Iterable) -> None:
                 row[index] = 'None'
                 
         print(f'Time:{row[0]:25}Action:{row[1]:25}Member:{row[2]:25}Item:{row[3]:25}Roll:{row[4]:25}')
+
+def encounterSplitter(data: Iterable) -> list:
+    output = []
+    newEncounter = True
+    for row in data:
+        if (newEncounter and row[2] == "ObtainLoot" and row[3] == "Your Character"):
+            newEncounter = False
+            encounter = []
+            encounter.append(row)
+            addedLoot = list()
+            # Loot obtained without an AddLoot is personal loot and should be not be considered to be obtainedLoot
+            obtainedLoot = list()
+        elif (newEncounter and row[2] == "AddLoot" and row[3] == ''):
+            newEncounter = False
+            encounter = []
+            encounter.append(row)
+            addedLoot = list(row[4])
+            obtainedLoot = list()
+        elif (not newEncounter and row[2] == "AddLoot" and row[3] == ''):
+            addedLoot.append(row[4])
+            encounter.append(row)
+        elif (not newEncounter and row[2] == "ObtainLoot" and len(addedLoot) > 0):
+            obtainedLoot.append(row[4])
+            encounter.append(row)
+            if len(Counter(obtainedLoot) - Counter(addedLoot)) == 0:
+                newEncounter = True
+                output.append(encounter)
+        else:
+            encounter.append(row)
+    return output
+
