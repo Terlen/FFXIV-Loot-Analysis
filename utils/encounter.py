178915523f05bb4ab45426c5271027d80526dbde
Aidan Payne <aidan@apayne.net>
2021-07-15 02:25:28 -0700

    Added add_member method and call from set_members
    Added class attributes
    
    Added __hash__ and __eq__ methods to Member class to prevent duplicate objects from being added to a set

--- utils/encounter.py
+++ utils/encounter.py
@@ -12,23 +12,39 @@ class Item:
 class Member:
     def __init__(self, name):
         self.name = name
+    def __hash__(self):
+        return hash(self.name)
+    def __eq__(self, other):
+        return self.name == other.name
 
 
 class Encounter:
+
+    rows = []
+    cleartime = ''
+    members = set()
+    loot = set()
+    rolls = set()
+
     def set_cleartime(self, firstrow):
         self.cleartime = firstrow[0]
     
+    def add_member(self, name):
+        self.members.add(Member(name))
+        return self.members
+
     def set_members(self, data):
-        self.members = set()
         for row in data:
             if row[2] != '':
-                self.members.add(row[2])
+                return self.add_member(row[2])
+        
     
-    # def 
+    #def 
 
     def __init__(self, data):
         self.rows = data
         self.set_cleartime(self.rows[0])
+        self.set_members(self.rows)
 
 
 
