5258084396bf119904f87739d9b0bd84b9444b8c
Aidan Payne <aidan@apayne.net>
2021-07-18 21:29:59 -0700

    Wrote basic functionality for get_item_fewest_rolls
    Passes 1/1 tests

--- utils/encounterAnalysis.py
+++ utils/encounterAnalysis.py
@@ -4,3 +4,8 @@ def get_item_most_rolls(encounter: Encounter) -> Item:
     itemCounts = [len(item.rolls) for item in encounter.items]
     maxRollsIndex = itemCounts.index(max(itemCounts))
     return encounter.items[maxRollsIndex]
+
+def get_item_fewest_rolls(encounter: Encounter) -> Item:
+    itemCounts = [len(item.rolls) for item in encounter.items]
+    minRollsIndex = itemCounts.index(min(itemCounts))
+    return encounter.items[minRollsIndex]
