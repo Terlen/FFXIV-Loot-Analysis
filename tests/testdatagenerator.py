e9e4c965dd27995d69d471af10a023503fa35ba7
Aidan Payne <aidan@apayne.net>
2021-07-27 23:31:12 -0700

    Refactored generator functions
    Because these functions are generating INPUT data, they don't want to generate objects
    
    They help create "rows" of data like a csv, datareader will handle object creation and processing

--- tests/testdatagenerator.py
+++ tests/testdatagenerator.py
@@ -11,30 +11,25 @@ lastNames = ['Cookiepouch', 'Final', 'Fantasy', 'Chocobo', 'Tonberry']
 itemAdjective = ["Tataru's", "Kugane", "Meaty", "Rusty", "Resplendent"]
 itemNoun = ["Stick", "Lance", "Pot", "Scissors", "Sword"]
 
-def random_member_gen():
+def random_name_gen():
     name = firstNames[fixedGen.randint(0,4)] + ' ' + lastNames[fixedGen.randint(0,4)]
-    return Member(name)
+    return name
 
 # Generate a random Item with random name and quantity between 1-3. Each item has no associated rolls on init
 def random_item_gen():
     name = itemAdjective[fixedGen.randint(0,4)] + ' ' + itemNoun[fixedGen.randint(0,4)]
     quantity = fixedGen.randint(1,3)
-    item = Item(name, quantity)
+    item = (name,quantity)
     return item
 
 # Generate a random roll for given item. Specify if roll is Need/Greed and if it won
-def random_roll_gen(rolltype : str, item : Item, member : Member = random_member_gen()):
-        roll = Roll(rolltype,member,fixedGen.randint(1,99),item)
-        item.rolls.append(roll)
-        #print (roll.member.name, roll.type, roll.value)
+def random_roll_gen():
+        rolltype = fixedGen.randint(0,2)
+        if rolltype == 1:
+            roll = ("GreedLoot", fixedGen.randint(1,99))
+        elif rolltype == 2:
+            roll = ("NeedLoot", fixedGen.randint(1,99))
         return roll
 
 
-# def test_data_gen(maxRolls, numItems, numMembers):
-#     data = []
-    
-#     items = []
-#     for x in range(numItems):
-#         items.append(random_item_gen())
 
-#     return items
