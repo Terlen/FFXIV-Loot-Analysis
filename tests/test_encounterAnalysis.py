9e791838e1b8fae3be8a5b42cb902e3a17300228
Aidan Payne <aidan@apayne.net>
2021-07-30 00:50:38 -0700

    Added tests for get_item_most_rolls

--- tests/test_encounterAnalysis.py
+++ tests/test_encounterAnalysis.py
@@ -1,25 +1,55 @@
 from utils.encounterAnalysis import get_item_most_rolls, get_item_fewest_rolls
 from utils.encounter import Encounter, Item, Member, Roll
-from testdatagenerator import random_item_gen, random_roll_gen
+from testdatagenerator import random_data_gen
+from utils.dataReader import encounterSplitter
 
 
 
 class Test_get_item_most_rolls_Unit:
-    pass
-    # items = [random_item_gen(),random_item_gen()]
+    
+    test_data_no_tie = [
+            ['8-8-08', 'AddLoot', '', 'Resplendent Pot', 0, 1], 
+            ['8-8-08', 'AddLoot', '', 'Resplendent Stick', 0, 1], 
+            ['8-8-08', 'CastLoot', 'Hien Fantasy', 'Resplendent Pot', 0, 1], 
+            ['8-8-08', 'CastLoot', 'Akiva Chocobo', 'Resplendent Pot', 0, 1], 
+            ['8-8-08', 'CastLoot', 'Hien Cookiepouch', 'Resplendent Pot', 0, 1], 
+            ['8-8-08', 'CastLoot', 'Hien Fantasy', 'Resplendent Stick', 0, 1], 
+            ['8-8-08', 'CastLoot', 'Akiva Chocobo', 'Resplendent Stick', 0, 1], 
+            ['8-8-08', 'CastLoot', 'Hien Cookiepouch', 'Resplendent Stick', 0, 1],  
+            ['8-8-08', 'GreedLoot', 'Akiva Chocobo', 'Resplendent Pot', 45, 1], 
+            ['8-8-08', 'GreedLoot', 'Hien Cookiepouch', 'Resplendent Pot', 41, 1], 
+            ['8-8-08', 'ObtainLoot', 'Akiva Chocobo', 'Resplendent Pot', 45, 1], 
+            ['8-8-08', 'NeedLoot', 'Hien Fantasy', 'Resplendent Stick', 25, 1], 
+            ['8-8-08', 'NeedLoot', 'Akiva Chocobo', 'Resplendent Stick', 9, 1], 
+            ['8-8-08', 'NeedLoot', 'Hien Cookiepouch', 'Resplendent Stick', 43, 1], 
+            ['8-8-08', 'ObtainLoot', 'Akiva Chocobo', 'Resplendent Stick', 45, 1]
+            ]
 
-    # items[0].rolls.append(random_roll_gen(1,1))
-    # items[0].rolls.append(random_roll_gen(1,0))
+    test_data_tie = [
+        ['8-8-08', 'AddLoot', '', 'Resplendent Pot', 0, 1], 
+        ['8-8-08', 'AddLoot', '', 'Resplendent Stick', 0, 1], 
+        ['8-8-08', 'CastLoot', 'Hien Fantasy', 'Resplendent Pot', 0, 1], 
+        ['8-8-08', 'CastLoot', 'Akiva Chocobo', 'Resplendent Pot', 0, 1], 
+        ['8-8-08', 'CastLoot', 'Hien Cookiepouch', 'Resplendent Pot', 0, 1], 
+        ['8-8-08', 'CastLoot', 'Hien Fantasy', 'Resplendent Stick', 0, 1], 
+        ['8-8-08', 'CastLoot', 'Akiva Chocobo', 'Resplendent Stick', 0, 1], 
+        ['8-8-08', 'CastLoot', 'Hien Cookiepouch', 'Resplendent Stick', 0, 1], 
+        ['8-8-08', 'GreedLoot', 'Hien Fantasy', 'Resplendent Pot', 13, 1], 
+        ['8-8-08', 'GreedLoot', 'Akiva Chocobo', 'Resplendent Pot', 45, 1], 
+        ['8-8-08', 'GreedLoot', 'Hien Cookiepouch', 'Resplendent Pot', 41, 1], 
+        ['8-8-08', 'ObtainLoot', 'Akiva Chocobo', 'Resplendent Pot', 45, 1], 
+        ['8-8-08', 'NeedLoot', 'Hien Fantasy', 'Resplendent Stick', 25, 1], 
+        ['8-8-08', 'NeedLoot', 'Akiva Chocobo', 'Resplendent Stick', 9, 1], 
+        ['8-8-08', 'NeedLoot', 'Hien Cookiepouch', 'Resplendent Stick', 43, 1], 
+        ['8-8-08', 'ObtainLoot', 'Akiva Chocobo', 'Resplendent Stick', 45, 1]
+        ]
 
-    # items[1].rolls.append(random_roll_gen(0, 1))
-    # items[1].rolls.append(random_roll_gen(0,0))
-    # items[1].rolls.append(random_roll_gen(0,0))
-
-    # encounter = Encounter([['18:00', 'AddLoot', '','Stick',0,1], ['18:00', 'GreedLoot', 'Test','Stick',20,1]])
-    # encounter.items = items
-
-    # def test_get_item_most_rolls(self):
-    #     assert get_item_most_rolls(self.encounter) == self.items[1]
-
-    # def test_get_item_fewest_rolls(self):
-    #     assert get_item_fewest_rolls(self.encounter) == self.items[0]
+    def test_get_item_most_rolls_no_tie(self):
+        test_encounter = Encounter(self.test_data_no_tie)
+        item_names = [item.name for item in test_encounter.items]
+        assert test_encounter.items[item_names.index("Resplendent Stick")] == get_item_most_rolls(test_encounter)
+    def test_get_item_most_rolls_tie(self):
+        test_encounter = Encounter(self.test_data_tie)
+        item_names = [item.name for item in test_encounter.items]
+        print(test_encounter.items)
+        assert test_encounter.items == get_item_most_rolls(test_encounter)
