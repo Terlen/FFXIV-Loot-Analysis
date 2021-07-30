a0e09a177c7bdcd0d8d842d2955789afc078c81d
Aidan Payne <aidan@apayne.net>
2021-07-30 01:31:31 -0700

    Added logic to handle multiple items with the same number of rolls

--- utils/encounterAnalysis.py
+++ utils/encounterAnalysis.py
@@ -1,9 +1,20 @@
 from utils.encounter import Encounter, Item, Roll, Member
+from collections import Counter
+from typing import Union
 
-def get_item_most_rolls(encounter: Encounter) -> Item:
-    itemCounts = [len(item.rolls) for item in encounter.items]
-    maxRollsIndex = itemCounts.index(max(itemCounts))
-    return encounter.items[maxRollsIndex]
+def get_item_most_rolls(encounter: Encounter) -> Union[Item,list]:
+    itemRollCounts = [len(item.rolls) for item in encounter.items]
+    rollCountCounter = Counter(itemRollCounts)
+    mostRolls = max(itemRollCounts)
+    if rollCountCounter[mostRolls] > 1:
+        # identify the indicies of itemRollCounts where itemRollCount[index] == mostRolls
+        # because the indicies of itemRollCounts correspond to encounter.items, we can use those indicies to return all the items with the most rolls
+        indiciesOfItems = [index for index, count in enumerate(itemRollCounts) if count == mostRolls]
+        return [encounter.items[index] for index in indiciesOfItems]
+    else:
+        maxRollsIndex = itemRollCounts.index(mostRolls)
+        return encounter.items[maxRollsIndex]
+
 
 def get_item_fewest_rolls(encounter: Encounter) -> Item:
     itemCounts = [len(item.rolls) for item in encounter.items]
