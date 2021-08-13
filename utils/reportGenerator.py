1e13c1ec324f9eec3cb34938eee39d507e78a782
Aidan Payne <aidan@apayne.net>
2021-08-13 03:46:27 -0700

    Added report section class and subclasses

--- utils/reportGenerator.py
+++ utils/reportGenerator.py
@@ -1,22 +1,58 @@
 from string import Template
 from inspect import cleandoc
 from typing import Union
+from collections.abc import Sequence
 
-def loadTemplate(templateDir: str):
-    with open(templateDir, 'r') as template:
-        format = Template(template.read())
-        template.close()
-    return format
+class Section:
+    
+    def __init__(self, title:str, nest:int =0, data=None):
+        self.title = "{heading} {title}\n".format(heading = '#'*(nest+1), title=title)
+        self.data = ''        
+        self.subSections = []
+    def addSubSection(self, subsection):
+        self.subSections.append(subsection)
 
-def listFormatter(iterable : Union[list,set]):
-    return ''.join('\n- {}'.format(item) for item in iterable)
+class ListSection(Section):
+    def __init__(self, title:str, nest:int =1, data=None):
+        super().__init__(title,nest)
+        if data != None:
+            self.data = ''.join('- {}\n'*len(data)).format(*data)
 
-def reportBuilder(filename:str, logger:str, members:set, template: Template):
-    templateMap = {}
-    templateMap['file'] = filename
-    templateMap['logger'] = logger
-    templateMap['members'] = listFormatter(members)
-    return template.substitute(templateMap)
+class ValueSection(Section):
+    def __init__(self, title: str, nest: int =1, data=None):
+        super().__init__(title, nest)
+        if data != None:
+            self.data = ''.join('{:.2f}\n').format(data)
+
+class GraphicSection(Section):
+    def __init__(self, title: str, nest: int=1, data=None):
+        super().__init__(title, nest)
+        if data != None:
+            self.data = "![Graph of roll distributions]({})\n".format(data)
+
+        
+
+
+class Report:
+    
+
+    def loadTemplate(templateDir: str):
+        with open(templateDir, 'r') as template:
+            format = Template(template.read())
+            template.close()
+        return format
+
+    def listFormat(iterable : Union[list,set]):
+        return ''.join('\n- {}'.format(item) for item in iterable)
+
+    
+    def __init__(self, template: str):
+        self.template = self.loadTemplate(template)
+        self.templateMap = {}
+    
+def reportBuilder(flags:list):
+    header = Section('header', )
+    
 
 
 def reportSave(outfolder, data):
