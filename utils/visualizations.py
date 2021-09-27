fc895dfb2dd8b329cee725ac9093842e330cf9c2
Aidan Payne <aidan@apayne.net>
2021-09-26 23:58:56 -0700

    Added images to pie chart wedges

--- utils/visualizations.py
+++ utils/visualizations.py
@@ -2,6 +2,9 @@ from matplotlib import ticker
 import matplotlib.pyplot as plt
 from typing import Counter
 from utils.aggregateAnalysis import stats
+from os import listdir
+from matplotlib.patches import PathPatch
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
  # Plot roll distribution
 def rollDistributionChart(rolledNumberCount: Counter, rollStatistics: stats, outputFolder: str, rollGraphFile: str) -> None:
@@ -37,11 +40,38 @@ def rollDistributionChart(rolledNumberCount: Counter, rollStatistics: stats, out
     except OSError:
         print("Unable to save roll distribution chart. Is the file open?")
 
+def img_to_pie( fn, wedge, xy, zoom=1, ax = None):
+    if ax==None: ax=plt.gca()
+    im = plt.imread(fn, format='png')
+    path = wedge.get_path()
+    patch = PathPatch(path, facecolor='none')
+    ax.add_patch(patch)
+    imagebox = OffsetImage(im, zoom=zoom, clip_path=patch, zorder=-10)
+    ab = AnnotationBbox(imagebox, xy, xycoords='data', pad=0, frameon=False)
+    ax.add_artist(ab)
+
+positions = [(0,0),(0,0),(0,0),(0,0)]
+zooms = [0.3,0.3,0.3,0.3]
+positions = positions*4
+zooms = zooms*4
+
+
+
 def pieChartPercentWins(winPercents: dict, outputFolder: str, pieChartFile: str) -> None:
     labels = sorted([key for key in winPercents.keys()], key= lambda key: winPercents[key])
     sizes = [value[2] for value in winPercents.values()]
     sizes.sort()
     fig2, ax2 = plt.subplots()
-    ax2.pie(sizes,labels=labels, autopct='%1.0f%%', startangle=90, wedgeprops={'color':(1.0,1.0,1.0,0.0), 'edgecolor':'k'}, pctdistance=0.8, counterclock=False)
+    wedges, text, label = ax2.pie(sizes,labels=labels, autopct='%1.0f%%', startangle=90, wedgeprops={'color':(1.0,1.0,1.0,0.0), 'edgecolor':'k'}, pctdistance=0.8, counterclock=False)
     ax2.axis('equal')
+    images = listdir(".\docs\cookies")
+    images = images*4
+    for i in range(len(labels)):
+        fn = "docs/cookies/{}".format(images[i])
+        img_to_pie(fn, wedges[i], xy=positions[i], zoom = zooms[i])
+        wedges[i].set_zorder(10)
+        text[i].set_zorder(10)
+        label[i].set_zorder(10)
+
     plt.savefig(outputFolder+pieChartFile, transparent=True, bbox_inches='tight')
+
